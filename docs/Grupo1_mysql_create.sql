5a779dd5c5787c6b0be1e6aa9b5c3776d27f2976
Niashi <mathias.pn7@gmail.com>
2021-04-30 21:45:57 -0300

    Alterando a documentação

--- docs/Grupo1_mysql_create.sql
+++ docs/Grupo1_mysql_create.sql
@@ -1,30 +1,29 @@
-CREATE TABLE `Produto` (
+CREATE TABLE `tb_produto` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`Nome` varchar(255) NOT NULL,
-	`Descricao` varchar(255) NOT NULL,
-	`Quantidade` INT(5) NOT NULL,
-	`Doado` BOOLEAN NOT NULL,
-	`Categoria_id` INT(5) NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`descricao` varchar(255) NOT NULL,
+	`quantidade` INT(5) NOT NULL,
+	`doado` BOOLEAN NOT NULL,
+	`categoria_id` INT(5) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Categoria` (
+CREATE TABLE `tb_usuario` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`Consulta` varchar(50) NOT NULL,
-	`Descartavel` BOOLEAN NOT NULL,
-	`Reutilizavel` BOOLEAN NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`senha` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `Usuario` (
+CREATE TABLE `tb_categoria` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`Nome` varchar(255) NOT NULL,
-	`Email` varchar(255) NOT NULL,
-	`Senha` varchar(255) NOT NULL,
+	`descricao` varchar(500) NOT NULL,
+	`reut_descart` BOOLEAN NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `Produto` ADD CONSTRAINT `Produto_fk0` FOREIGN KEY (`Doado`) REFERENCES `Usuario`(`id`);
+ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`doado`) REFERENCES `tb_usuario`(`id`);
 
-ALTER TABLE `Produto` ADD CONSTRAINT `Produto_fk1` FOREIGN KEY (`Categoria_id`) REFERENCES `Categoria`(`id`);
+ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk1` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria`(`id`);
 
