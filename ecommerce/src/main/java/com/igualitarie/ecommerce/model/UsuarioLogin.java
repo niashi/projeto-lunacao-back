1ea1fbbf4d0e1e1211587714dc5282ab627ff77f
HugoSaladini <hg.saladini@gmail.com>
2021-06-02 10:57:45 -0300

    Version final 1.0

--- ecommerce/src/main/java/com/igualitarie/ecommerce/model/UsuarioLogin.java
+++ ecommerce/src/main/java/com/igualitarie/ecommerce/model/UsuarioLogin.java
@@ -1,6 +1,8 @@
 package com.igualitarie.ecommerce.model;
 
 public class UsuarioLogin {
+	
+	private long id;
 
 	private String nome;
 
@@ -14,6 +16,17 @@ public class UsuarioLogin {
 	
 	private String tipo;
 	
+
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
