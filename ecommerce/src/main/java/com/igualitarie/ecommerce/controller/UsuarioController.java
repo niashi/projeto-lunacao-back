2c1b1c77dafacb06b22fb1f5219b56742a8fe3fc
agnidmitri <limadmitri@protonmail.com>
2021-06-09 14:43:30 -0300

    inclusao de getByIdUser

--- ecommerce/src/main/java/com/igualitarie/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/com/igualitarie/ecommerce/controller/UsuarioController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.igualitarie.ecommerce.model.Usuario;
 import com.igualitarie.ecommerce.model.UsuarioLogin;
+import com.igualitarie.ecommerce.repository.UsuarioRepository;
 import com.igualitarie.ecommerce.service.UsuarioService;
 
 @RestController
@@ -23,6 +26,10 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+    private UsuarioRepository repository;
+	
 
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
@@ -34,6 +41,11 @@ public class UsuarioController {
 		}
 
 	}
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
 
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user) {
